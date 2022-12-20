ecce1e10a7edc0693a8bb27bb417216969fbc60c
Anish kumar Srinivaasan <anishkumar92@gmail.com>
2022-12-20 22:38:43 +0530

    code cleanup

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
-
+navItems = [nav1,nav2,nav3,nav4,nav5];
 // toggle menu 
 function toggleNav() {
     menuBars.classList.toggle('change');
@@ -43,8 +43,6 @@ function toggleNav() {
 }
 // event listners
 menuBars.addEventListener('click',toggleNav);
-nav1.addEventListener('click',toggleNav);
-nav2.addEventListener('click',toggleNav);
-nav3.addEventListener('click',toggleNav);
-nav4.addEventListener('click',toggleNav);
-nav5.addEventListener('click',toggleNav);
+navItems.forEach(nav => {
+    nav.addEventListener('click',toggleNav);
+});
